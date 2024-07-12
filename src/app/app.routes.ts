2f68f88f548f622255f43b18a893abefeca0d010
acmmp <adriancampayo6@gmail.com>
2024-07-12 14:11:26 +0200

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './features/products/products.component';
+import { ContactoComponent } from './features/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'Productos', component: ProductsComponent },
+    { path: 'Contacto', component: ContactoComponent }
+];
